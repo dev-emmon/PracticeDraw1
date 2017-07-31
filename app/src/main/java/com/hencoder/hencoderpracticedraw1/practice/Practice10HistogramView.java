c20d483acb1b11857e0e7849d38bf562f0e202b1
Em <liz3002@126.com>
2017-07-31 12:02:17 +0800

    complete all

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
@@ -2,6 +2,8 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -26,5 +28,34 @@ public class Practice10HistogramView extends View {
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画直方图
+
+        Paint paint1 = new Paint();
+        paint1.setColor(Color.WHITE);
+        canvas.drawLine(100, 30, 100, 500, paint1);
+        canvas.drawLine(100, 500, 1000, 500, paint1);
+
+        int start = 100;
+        int xspace = 40;
+        int width = 100;
+        Paint paint2 = new Paint();
+        paint2.setColor(Color.GREEN);
+        canvas.drawRect(start + xspace, 400, start + xspace + width, 498, paint2);
+        canvas.drawRect(start + xspace * 2 + width * 1, 450, start + (xspace + width) * 2, 498, paint2);
+        canvas.drawRect(start + xspace * 3 + width * 2, 100, start + (xspace + width) * 3, 498, paint2);
+        canvas.drawRect(start + xspace * 4 + width * 3, 200, start + (xspace + width) * 4, 498, paint2);
+        canvas.drawRect(start + xspace * 5 + width * 4, 250, start + (xspace + width) * 5, 498, paint2);
+        canvas.drawRect(start + xspace * 6 + width * 5, 150, start + (xspace + width) * 6, 498, paint2);
+
+        Paint paint3 = new Paint();
+        paint3.setTextSize(24);
+        paint3.setTextAlign(Paint.Align.CENTER);
+        paint3.setColor(Color.WHITE);
+        canvas.drawText("Froyo", start + xspace + width / 2, 520, paint3);
+        canvas.drawText("GB", start + xspace * 2 + width * 1 + width / 2, 520, paint3);
+        canvas.drawText("ICS", start + xspace * 3 + width * 2 + width / 2, 520, paint3);
+        canvas.drawText("JB", start + xspace * 4 + width * 3 + width / 2, 520, paint3);
+        canvas.drawText("Kitkat", start + xspace * 5 + width * 4 + width / 2, 520, paint3);
+        canvas.drawText("L", start + xspace * 6 + width * 5 + width / 2, 520, paint3);
+
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
@@ -2,6 +2,10 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -26,5 +30,68 @@ public class Practice11PieChartView extends View {
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画饼图
+
+        int radius = 240;
+        drawCake(canvas, 500 - 20, 300 - 20, radius, 180, 300, "Lollipop", Color.GRAY, Color.RED);
+        drawCake(canvas, 500, 300, radius, 305, 360, "M", Color.GRAY, Color.YELLOW);
+        drawCake(canvas, 500, 300, radius, 2, 10, "F", Color.GRAY, Color.LTGRAY);
+        drawCake(canvas, 500, 300, radius, 12, 20, "I", Color.GRAY, Color.GREEN);
+        drawCake(canvas, 500, 300, radius, 22, 70, "J", Color.GRAY, Color.BLUE);
+        drawCake(canvas, 500, 300, radius, 73, 180, "K", Color.GRAY, Color.CYAN);
+    }
+
+    private void drawCake(Canvas canvas, int x, int y, int radius, int startDegree, int endDegree, String text, int lineColor, int sakeColor) {
+        int l = 20;
+        Paint paintLine = new Paint();
+        paintLine.setColor(lineColor);
+        paintLine.setStrokeWidth(3);
+        if (startDegree < 0) {
+            startDegree = 360 + startDegree;
+        }
+        if (endDegree < 0) {
+            endDegree = 360 + endDegree;
+        }
+        int midD = (startDegree + endDegree) / 2;
+        Paint paintText = new Paint();
+        paintText.setColor(Color.WHITE);
+        paintText.setTextSize(24);
+        if (0 <= midD && midD < 90) {
+            double radians = Math.toRadians(midD % 90);
+            float endx = (float) (x + Math.cos(radians) * (radius + l));
+            float endy = (float) (y + Math.sin(radians) * (radius + l));
+            canvas.drawLine(x, y, endx, endy, paintLine);
+            canvas.drawLine(endx, endy, endx + l * 3, endy, paintLine);
+            paintText.setTextAlign(Paint.Align.LEFT);
+            canvas.drawText(text, endx + (l * 3 + 20), endy, paintText);
+        } else if (90 <= midD && midD < 180) {
+            double radians = Math.toRadians(midD % 90);
+            float endx = (float) (x - Math.sin(radians) * (radius + l));
+            float endy = (float) (y + Math.cos(radians) * (radius + l));
+            canvas.drawLine(x, y, endx, endy, paintLine);
+            canvas.drawLine(endx, endy, endx - l * 3, endy, paintLine);
+            paintText.setTextAlign(Paint.Align.RIGHT);
+            canvas.drawText(text, endx - (l * 3 + 20), endy, paintText);
+        } else if (180 <= midD && midD < 270) {
+            double radians = Math.toRadians(midD % 90);
+            float endx = (float) (x - Math.cos(radians) * (radius + l));
+            float endy = (float) (y - Math.sin(radians) * (radius + l));
+            canvas.drawLine(x, y, endx, endy, paintLine);
+            canvas.drawLine(endx, endy, endx - l * 3, endy, paintLine);
+            paintText.setTextAlign(Paint.Align.RIGHT);
+            canvas.drawText(text, endx - (l * 3 + 20), endy, paintText);
+        } else if (270 <= midD && midD < 360) {
+            double radians = Math.toRadians(midD % 90);
+            float endx = (float) (x + Math.sin(radians) * (radius + l));
+            float endy = (float) (y - Math.cos(radians) * (radius + l));
+            canvas.drawLine(x, y, endx, endy, paintLine);
+            canvas.drawLine(endx, endy, endx + l * 3, endy, paintLine);
+            paintText.setTextAlign(Paint.Align.LEFT);
+            canvas.drawText(text, (endx + l * 3 + 20), endy, paintText);
+        }
+
+        Paint paint2 = new Paint();
+        paint2.setColor(sakeColor);
+        RectF rectF = new RectF(x - radius, y - radius, x + radius, y + radius);
+        canvas.drawArc(rectF, startDegree, endDegree - startDegree, true, paint2);
     }
 }
